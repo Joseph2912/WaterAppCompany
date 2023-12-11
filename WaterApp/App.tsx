6a9ef0f8bd1702910c6f83354bb12aaff008bfc3
Joseph2912 <elsjoseph@hotmail.com>
2023-12-11 09:42:45 -0500

    First commit

---
+++ WaterApp/App.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import { View } from "react-native";
+import Login from './src/App'
+
+export default class App extends Component{
+
+    render(){
+        return( <Login/> )
+        
+    }
+
+}
---
+++ WaterApp/windows/WaterApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::WaterApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::WaterApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
